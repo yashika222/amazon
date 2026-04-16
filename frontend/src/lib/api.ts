76cdc21e4798bc5ec75344ca5232ba9027199b00
yashika <yashika2222255@gmail.com>
2026-04-16 20:46:03 +0530

    fix build error

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
-  timeout: 20000, // 20 seconds instead of 5 seconds
+  baseURL: "https://amazon-backend-zdki.onrender.com"
 });
 
 export default API;
